d696b809be9cfd2203521fe92ee378896bd8651b
eelve <i@eelve.com>
2019-08-18 16:55:54 +0800

    添加配置服务

---
+++ lovin-config-server/src/main/java/com/eelve/lovin/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.eelve.lovin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @ClassName WebSecurityConfig
+ * @Description TDO
+ * @Author zhao.zhilue
+ * @Date 2019/8/18 13:52
+ * @Version 1.0
+ **/
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll()
+                .and().csrf().disable();
+    }
+}
