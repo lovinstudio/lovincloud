e828382c2d6421928614f56a682857da7c90fcd6
eelve <zzlve@foxmail.com>
2020-09-25 17:41:11 +0800

    初始化lovin-cloud-sleuth项目

---
+++ lovin-cloud-sleuth/src/main/java/com/eelve/lovin/LovinCloudSleuthApplication.java
@@ -0,0 +1,16 @@
+package com.eelve.lovin;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName LovinCloudSleuthApplication
+ * @Description TDO
+ * @Author zhao.zhilue
+ * @Date 2020/9/25 17:34
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LovinCloudSleuthApplication {
+}
